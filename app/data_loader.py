23b37dddbe674e1514a75d199f7c11cd088ceefa
Amelie-Mad <madrona.amelie@gmail.com>
2025-05-23 20:20:34 +0200

    folders

--- app/data_loader.py
+++ app/data_loader.py
@@ -3,12 +3,12 @@ import streamlit as st
 
 @st.cache_data
 def load_books_data():
-    df = pd.read_csv("items_enhanced_final.csv")
+    df = pd.read_csv("enhancing/items_enhanced_final.csv")
     return df
 
 @st.cache_data
 def load_recommendations():
-    df = pd.read_csv("hybrid_recommendations.csv")
+    df = pd.read_csv("app/hybrid_recommendations.csv")
     return df.set_index("user_id")["recommendation"].to_dict()
 
 @st.cache_data
